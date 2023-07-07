dd5db2c8cccbeb307ec60c849dead6d1c9e98553
zolan30 <mehtaaryan207@gmail.com>
2023-07-07 22:53:28 +0530

    Encryption Layer Added

--- src/main/java/com/mycompany/dataPacks/data_encryption.java
+++ src/main/java/com/mycompany/dataPacks/data_encryption.java
@@ -13,21 +13,26 @@ import org.apache.xerces.impl.dv.util.Base64;
 public class data_encryption {
     
     private static final String ALGO = "AES";
-    private static final byte[]ENC_KEY = "a1b2c3d4e5f6g7h8".getBytes();
+    private static final byte[]AES_ENC_KEY = "a1b2c3d4e5f6g7h8".getBytes();
     
+    private static final int ceaser_KEY = 21;
+    
+    
+    //CEASER->AES
     public static String encrypt(String plain_text){
-        return text_encrypt(plain_text);
+        return text_encrypt(ceaser_encrypt(plain_text));
     }
     
+    //AES->CEASER
     public static String decrypt(String cipher_text){
-        return text_decrypt(cipher_text);
+        return ceaser_decrypt(text_decrypt(cipher_text));
     }
     
     private static String text_encrypt(String plain_text){
         String cipher_text = null;
         
         try {
-            Key key = new SecretKeySpec(ENC_KEY,ALGO);
+            Key key = new SecretKeySpec(AES_ENC_KEY,ALGO);
             Cipher cipher = Cipher.getInstance(ALGO);
             cipher.init(Cipher.ENCRYPT_MODE,key);
             
@@ -45,7 +50,7 @@ public class data_encryption {
         byte[] plain_text = new byte[0];
         
         try {
-            Key key = new SecretKeySpec(ENC_KEY, ALGO);
+            Key key = new SecretKeySpec(AES_ENC_KEY, ALGO);
             Cipher cipher = Cipher.getInstance(ALGO);
             cipher.init(Cipher.DECRYPT_MODE,key);
             
@@ -56,5 +61,37 @@ public class data_encryption {
             e.printStackTrace();
         }
         return new String(plain_text);
+    }
+    
+    private static String ceaser_encrypt(String plain_text){
+        StringBuilder cipher_text = new StringBuilder();
+        
+        for(int i=0;i<plain_text.length();i++){
+            char ch = plain_text.charAt(i);
+            if(Character.isLetter(ch)){
+                char base = Character.isLowerCase(ch) ? 'a' : 'A';
+                char shiftedChar = (char) (((ch - base + ceaser_KEY) % 26) + base);
+                cipher_text.append(shiftedChar);
+            }else{
+                cipher_text.append(ch);
+            }
+        }
+        return cipher_text.toString();
+    }
+    
+    private static String ceaser_decrypt(String cipher_text){
+        StringBuilder plain_text = new StringBuilder();
+        
+        for(int i=0;i<cipher_text.length();i++){
+            char ch = cipher_text.charAt(i);
+            if (Character.isLetter(ch)) {
+                char base = Character.isLowerCase(ch) ? 'a' : 'A';
+                char shiftedChar = (char) (((ch - base - ceaser_KEY + 26) % 26) + base);
+                plain_text.append(shiftedChar);
+            } else {
+                plain_text.append(ch);
+            }
+        }
+        return plain_text.toString();
     }
 }
